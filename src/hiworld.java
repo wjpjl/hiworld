ad56bf5a63ce7bfc9176f5875ad3660bca0cf890
Administrator <1454590073@qq.com>
2021-03-21 15:32:51 +0800

    test

---
+++ src/hiworld.java
@@ -0,0 +1,10 @@
+/**
+ * @author pjl
+ * @version 1.0
+ * @date 2021/3/21 0021 15:29
+ */
+public class hiworld {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
